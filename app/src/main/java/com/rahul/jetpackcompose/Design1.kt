966cdf0c6349610f83cdc0caa18c39bc0cce57f1
Rahul <rahulbodh819@gmail.com>
2025-02-25 18:53:40 +0530

    create alignYourBodyUI

--- app/src/main/java/com/rahul/jetpackcompose/Design1.kt
+++ app/src/main/java/com/rahul/jetpackcompose/Design1.kt
@@ -1,14 +1,20 @@
 package com.rahul.jetpackcompose
 
-import androidx.compose.material.icons.Icons
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
-import androidx.compose.material.icons.filled.KeyboardArrowDown
-import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -16,7 +22,13 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.motionEventSpy
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -27,7 +39,7 @@ class Design1 : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposeTheme {
-                SearchBar()
+                AlignYourBodyElement(text = R.string.name , drawable = R.drawable.img1 , modifier = Modifier.padding(24.dp))
             }
         }
     }
@@ -59,10 +71,37 @@ fun SearchBar(
     )
 }
 
-@Preview
+
 @Composable
-fun searchBarPreview(){
+fun AlignYourBodyElement(
+    @DrawableRes drawable: Int,
+    @StringRes text: Int,
+    modifier: Modifier
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(drawable),
+            contentDescription = null,
+            modifier = Modifier
+                .size(88.dp)
+                .clip(CircleShape),
+            contentScale = ContentScale.Crop
+        )
+        Text(text = stringResource(text) ,
+            modifier = Modifier.paddingFromBaseline(top = 24.dp, bottom = 8.dp),
+            style = MaterialTheme.typography.bodyMedium,
+            color = Color.Blue )
+    }
+}
+
+@Preview()
+@Composable
+fun searchBarPreview() {
     JetpackComposeTheme {
-        SearchBar()
+        AlignYourBodyElement(text = R.string.name , drawable = R.drawable.img1 , modifier = Modifier.padding(24.dp))
+
     }
 }
