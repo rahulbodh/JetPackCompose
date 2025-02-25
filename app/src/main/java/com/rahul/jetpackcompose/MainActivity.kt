49bc3acae3ee72b14eee905aa795996d474ac4a8
Rahul <rahulbodh819@gmail.com>
2025-02-25 13:35:10 +0530

    expend the item using onclick

--- app/src/main/java/com/rahul/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/rahul/jetpackcompose/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.rahul.jetpackcompose
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -17,6 +18,8 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.internal.liveLiteral
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -48,16 +51,23 @@ fun MyApp(
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    val expended = remember { mutableStateOf(false) }
+    val extraPadding = if(expended.value) 48.dp else 0.dp
+
     Surface(color = MaterialTheme.colorScheme.primary,
     modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
         Row (modifier = Modifier.padding(24.dp)
         ){
-            Column(modifier = Modifier.weight(1f)) {
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(bottom = extraPadding)
+            ) {
                 Text(text = "Hello")
                 Text(text = name)
             }
-            ElevatedButton(onClick = { /*TODO*/ }) {
-                Text(text = "Show More")
+            ElevatedButton(onClick = { expended.value  = !expended.value }) {
+                Log.d("MainActivity", "Greeting: $expended")
+                Text(if(expended.value) "Show less" else "Show more")
             }
         }
     }
