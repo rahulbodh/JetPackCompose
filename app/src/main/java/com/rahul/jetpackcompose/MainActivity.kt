0fcbf15979468f912e7b6e06d3c26339ca24c02e
Rahul <rahulbodh819@gmail.com>
2025-02-25 14:47:39 +0530

    create a lazylist

--- app/src/main/java/com/rahul/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/rahul/jetpackcompose/MainActivity.kt
@@ -12,6 +12,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
@@ -78,12 +80,12 @@ fun MyApp(modifier: Modifier = Modifier) {
 @Composable
 private fun Greetings(
     modifier: Modifier = Modifier,
-    names: List<String> = listOf("Rahul", "Usman")
+    names: List<String> = List(1000) { "$it" }
 ) {
-    Column(modifier = modifier.padding(vertical = 4.dp)) {
-        for (name in names) {
-            Greeting(name = name)
-        }
+    LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
+      items(items = names ) { name ->
+          Greeting(name = name)
+      }
     }
 }
 
