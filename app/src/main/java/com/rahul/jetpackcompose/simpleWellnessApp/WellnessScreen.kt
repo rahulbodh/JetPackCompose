8141cff2032890261994bd99125bd97e9eed93d5
Rahul <rahulbodh819@gmail.com>
2025-02-27 16:44:14 +0530

    restructure the Wellness app

--- app/src/main/java/com/rahul/jetpackcompose/simpleWellnessApp/WellnessScreen.kt
+++ app/src/main/java/com/rahul/jetpackcompose/simpleWellnessApp/WellnessScreen.kt
@@ -12,35 +12,15 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.rahul.jetpackcompose.sootheApp.MySootheAppPortrait
 import com.rahul.jetpackcompose.ui.theme.JetpackComposeTheme
 
-class WellnessHome : ComponentActivity() {
+class WellnessScreen : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposeTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    WellnessScreen()
-                }
             }
         }
     }
-}
-
-@Composable
-fun WaterCounter(modifier: Modifier = Modifier) {
-    var count = 0
-
-    Text(
-        text = "You've had $count glasses.",
-        modifier = modifier.padding(16.dp)
-    )
-}
-
-@Composable
-fun WellnessScreen(modifier: Modifier = Modifier) {
-    WaterCounter(modifier)
 }
