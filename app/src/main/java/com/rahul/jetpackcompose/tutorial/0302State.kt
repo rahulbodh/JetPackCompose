f8c9b593e32a5598021bd64b26aef96e5b66465c
Rahul <rahulbodh819@gmail.com>
2026-06-04 11:32:41 +0530

    State in Compose

---
+++ app/src/main/java/com/rahul/jetpackcompose/tutorial/0302State.kt
@@ -0,0 +1,56 @@
+package com.rahul.jetpackcompose.tutorial
+
+import android.widget.Switch
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Switch
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.content.pm.ShortcutInfoCompat
+
+@Composable
+fun State0302(modifier: Modifier = Modifier) {
+
+    Column(
+        modifier = modifier.fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(
+            space = 48.dp,
+            alignment = Alignment.CenterVertically
+        ),
+        horizontalAlignment = Alignment.CenterHorizontally
+
+
+    ) {
+
+        var checked = true
+        var checkedState : MutableState<Boolean> = remember { mutableStateOf(true) }
+        Switch(
+            checked = checkedState.value,
+            onCheckedChange = {checkedState.value = !checkedState.value}
+        )
+
+        Checkbox(
+            checked = checked ,
+            onCheckedChange = {checked = !checked}
+        )
+    }
+    
+}
+
+@Preview
+@Composable
+private fun State0302Preview() {
+    State0302()
+}
